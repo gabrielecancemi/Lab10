c16bcaacc2850cf185c81092a8d4c568fd33496e
s326548 <gabrielecancemi2004@gmail.com>
2026-05-12 15:16:28 +0200

    fine

--- UI/controller.py
+++ UI/controller.py
@@ -7,6 +7,61 @@ class Controller:
         self._view = view
         # the model, which implements the logic of the program and holds the data
         self._model = model
+        self._partenza = None
 
     def handleCalcola(self, e):
-        pass
+        self._view._txt_result.controls.clear()
+        anno = self._view._txtAnno.value
+        if anno is None or anno == "":
+            self._view._txt_result.controls.append(ft.Text("Inserire un anno", color="red"))
+            self._view.update_page()
+            return
+        try:
+            anno = int(anno)
+        except:
+            self._view._txt_result.controls.append(ft.Text("Inserire un anno intero", color="red"))
+            self._view.update_page()
+            return
+
+        if anno < 1816 or anno > 2016:
+            self._view._txt_result.controls.append(ft.Text("Inserire un anno tra 1816 e 2016", color="red"))
+            self._view.update_page()
+            return
+
+        nodi = self._model.createGraph(anno)
+        self._view._txt_result.controls.append(ft.Text(f"Creato grafo con {self._model.conComponents()} componenti connesse", color="green"))
+        for n in nodi:
+            self._view._txt_result.controls.append(ft.Text(f"{n[0]} -- GRADO: {n[1]}"))
+
+        self.fillDD(nodi)
+
+        self._view.update_page()
+
+    def handleCerca(self, e):
+        self._view._txt_result.controls.clear()
+        if self._partenza is None:
+            self._view._txt_result.controls.append(ft.Text("Inserire uno stato di partenza", color="red"))
+            self._view.update_page()
+            return
+
+
+        nodi = self._model.statiRaggiungibili(self._partenza)
+        self._view._txt_result.controls.append(
+            ft.Text(f"Nodi raggiungibili da {self._partenza} ({len(nodi)}):", color="green"))
+        for n in nodi:
+            self._view._txt_result.controls.append(ft.Text(f"{n}"))
+
+        nodi = self._model.statiRaggiungibili2(self._partenza)
+        self._view._txt_result.controls.append(
+            ft.Text(f"Nodi raggiungibili da {self._partenza} ({len(nodi)}):", color="orange"))
+        for n in nodi:
+            self._view._txt_result.controls.append(ft.Text(f"{n}"))
+
+        self._view.update_page()
+
+    def fillDD(self, nodi):
+        for n in nodi:
+            self._view._ddStato.options.append(ft.dropdown.Option(text=n[0], data=n[0], on_click=self.salva))
+
+    def salva(self, e):
+        self._partenza = e.control.data
--- model/model.py
+++ model/model.py
@@ -6,4 +6,66 @@ from database.DAO import DAO
 class Model:
 
     def __init__(self):
-        pass
+        self._g = nx.Graph()
+        self._stati = DAO.getStati()
+        self._IdStati = dict()
+
+        for s in self._stati:
+            self._IdStati[s.CCode] = s
+
+
+    def createGraph(self, anno):
+        self._g.clear()
+        print(self.numEdges())
+        archi = DAO.getConfiniAnno(anno)
+
+        for a in archi:
+            u = self._IdStati.get(a[0])
+            v = self._IdStati.get(a[1])
+            t = a[2]
+            if int(t) != 1:
+                self._g.add_node(u)
+                self._g.add_node(v)
+                continue
+            if u is not None and v is not None:
+                self._g.add_edge(u, v)
+
+        print(self._g.nodes())
+        #print(self.numEdges())
+        res = []
+        for n in self._g.nodes():
+            res.append((n, self._g.degree(n)))
+        res.sort(key = lambda x : x[0].StateNme)
+        return(res)
+
+    def statiRaggiungibili(self, p):
+        daVisitare = [p]
+        visitati = self.ricorsione(daVisitare, [])
+        return visitati
+
+    def statiRaggiungibili2(self, p):
+        tree = nx.dfs_tree(self._g, p)
+        return tree.nodes()
+
+    def ricorsione(self, daVisitare, visitati):
+        if daVisitare:
+            print(len(visitati))
+            n = daVisitare[-1]
+            daVisitare.remove(n)
+            visitati.append(n)
+            vicini = self._g.neighbors(n)
+            for v in vicini:
+                if v not in visitati and v not in daVisitare:
+                    daVisitare.append(v)
+            self.ricorsione(daVisitare, visitati)
+
+        return visitati
+
+    def conComponents(self):
+        return nx.number_connected_components(self._g)
+
+    def numEdges(self):
+        return len(self._g.edges())
+
+    def numNodes(self):
+        return len(self._g.nodes())
